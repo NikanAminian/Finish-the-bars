fece0c716865821b2d1e619225f722a7106dff07
Nikan Aminian <nikan.aminian@gmail.com>
2017-07-27 01:24:39 -0700

    Fixed bugs, added dynamic gifs

--- js/quiz.js
+++ js/quiz.js
@@ -4,6 +4,7 @@
     Got me reminiscin' 'bout my old day. \
     Three 6, _____",
     song: "../resources/mp3/jodye.mp3",
+    gif: "../resources/gif/rocky.gif",
     choices: ["7,8,9", "suck a nigga dick, no foreplay, all day","come suck a dick"
     ,"Boomin' out the trap through the hallway"],
     correctAnswer: 1
@@ -13,6 +14,7 @@
     I got rap niggas that I gotta act like I like, \
     _____",
     song: "../resources/mp3/energy.mp3",
+    gif: "../resources/gif/drake.gif",
     choices: ["But y'all don't wanna see Win Win, 50 or Whoa", "Bout to call your ass a Uber, I got somewhere to be",
       "But I got bitches askin me about the code for the Wi-Fi","But my actin' days are over, fuck them niggas for life"],
     correctAnswer: 3
@@ -22,6 +24,7 @@
     I paid for that crib I never stepped foot in, \
     _______",
     song: "../resources/mp3/babyblue.mp3",
+    gif: "../resources/gif/action.gif",
     choices: ["And now somebody else is in there poopin", "And now my dashboard wooden",
       "And now somebody else is eating all the pudding","And now we out here cookin"],
     correctAnswer: 2
@@ -31,6 +34,7 @@
       I'm cocoo for the cocoa leaves and I turned it to dope, \
       The money never grew on trees but _________",
     song: "../resources/mp3/dope.mp3",
+    gif: "../resources/gif/q1.gif",
     choices: ["you might find me slappin' bones", "We talkin' cold watchin' hoes",
       "I'm watching it grow", "us lil' niggas love gold chains"],
     correctAnswer: 2
@@ -39,14 +43,40 @@
       I just had some bitches and I made 'em lip lock, \
       I just took a piss and _________",
     song: "../resources/mp3/drought.mp3",
+    gif: "../resources/gif/gucci.gif",
     choices: ["I seen codeine coming out", "choose the dirty over you",
       "thought it was a drought", "had to make me a cot"],
     correctAnswer: 0
+  },{
+    question: "Hi, kids! Do you like violence? \
+    Wanna see me stick nine inch nails through each one of my eyelids? \
+    Wanna copy me and do exactly like I did? \
+    Try 'cid and get fucked up worse than my life is \
+    My brain's dead weight, I'm trying to get my head straight \
+    But I can't figure out _________",
+    song: "../resources/mp3/mynameis.mp3",
+    gif: "../resources/gif/em.gif",
+    choices: ["What my name is", "which Spice Girl I want to impregnate",
+      "my original self from the top bunk with a belt", "why I ripped Pamela Lee's tits off"],
+    correctAnswer: 1
+  },{
+    question: "Only thing open is Waffle House, girl don't start with me, \
+      I used the Western Union for you like it's no prob, \
+      'Cause you was in college complainin' about it's no jobs, \
+      But you were suckin' a nigga dick the whole time, \
+      _________",
+    song: "../resources/mp3/30hours.mp3",
+    gif: "../resources/gif/kanye.gif",
+    choices: ["but me and wifey make a movie", "like bitch why you aint got no job",
+      "Well I guess a blowjob's better than no job", "I need a happy beginnin', middle and endin'"],
+    correctAnswer: 2
   }];
 
   var questionCounter = 0; //Tracks question number
   var selections = []; //Array containing user choices
   var quiz = $('#quiz'); //Quiz div object
+  var gifHolder = $('#gifHolder');
+  var audio = document.createElement('audio');
 
 
   // Display initial question
@@ -64,7 +94,7 @@
 
     // If no user selection, progress is stopped
     if (isNaN(selections[questionCounter])) {
-      alert('Choose goddamn answer!');
+      alert('Choose a goddamn answer first!');
     } else {
       questionCounter++;
       displayNext();
@@ -112,21 +142,20 @@
     });
 
     // Load and play the corresponding track bite
-    var audio = document.createElement('audio');
     audio.src = questions[index].song;
     audio.play();
 
-    // var header = $('<h2>Question ' + (index + 1) + ':</h2>');
-    //qElement.append(header);
-
+    // Display the question
     var question = $('<p>').append(questions[index].question);
     qElement.append(question);
 
-    // var graphics = $('<img src="sound.gif">');
-    // qElement.append(graphics);
-
+    // Display the questions
     var radioButtons = createRadios(index);
     qElement.append(radioButtons);
+
+    // Display the corresponding gif
+    var gif_src = '<img src="'+ questions[index].gif + '">';
+    gifHolder.append(gif_src);
 
     return qElement;
   }
@@ -153,6 +182,10 @@
 
   // Displays next requested element
   function displayNext() {
+
+    // Clear the gifHolder div for the next gif to append.
+    document.getElementById('gifHolder').innerHTML = "";
+
     quiz.fadeOut(function() {
       $('#question').remove();
 
@@ -183,6 +216,9 @@
 
   // Computes score and returns a paragraph element to be displayed
   function displayScore() {
+
+    audio.pause();
+
     var score = $('<p>',{id: 'question'});
 
     var numCorrect = 0;
@@ -192,11 +228,18 @@
       }
     }
 
-    if (numCorrect == 5) {
+    if (numCorrect == questions.length) {
       score.append('Goodjob Genuis.com, you got all of em. ');
+      // Display the corresponding gif
+      var gif_src = '<img src="../resources/gif/denzel.gif">';
+      gifHolder.append(gif_src);
     }else {
       score.append('You got ' + numCorrect + ' questions out of ' +
                    questions.length + ' right!!!');
+
+       // Display the corresponding gif
+       var gif_src = '<img src="../resources/gif/numbers.gif">';
+       gifHolder.append(gif_src);
     }
     return score;
   }
